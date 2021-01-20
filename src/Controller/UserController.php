8030a754fbda8c7e7bd258a3e644efb95027b8a1
Léo Delmas <ldelmas@axome.com>
2021-01-20 08:26:06 +0100

    [*] access_controle, securisation de /, exception /login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/app/users")
+ * @Route("/admin/users")
  */
 class UserController extends AbstractController
 {
--- src/Controller/WorkingDayController.php
+++ src/Controller/WorkingDayController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/app/planning")
+ * @Route("/planning")
  */
 class WorkingDayController extends AbstractController
 {
