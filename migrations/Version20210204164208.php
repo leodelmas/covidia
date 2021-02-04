1238859b694d432e557d5ac609e725959869e0b5
Léo Delmas <ldelmas@axome.com>
2021-02-04 17:48:26 +0100

    [*] Nettoyage migration + inté bouton SOS

--- migrations/Version20210204164208.php
+++ migrations/Version20210204164208.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210131144102 extends AbstractMigration
+final class Version20210204164208 extends AbstractMigration
 {
     public function getDescription() : string
     {
