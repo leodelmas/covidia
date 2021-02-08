689559f450214389915064c646069b41c0e8f677
Léo Delmas <ldelmas@axome.com>
2021-02-08 12:33:55 +0100

    [+] Ajout bootstrap à webpack + gestion du scss + adaptation front compatibilité bootstrap 5 > 4

--- assets/app.js
+++ assets/app.js
@@ -6,10 +6,13 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // start the Stimulus application
 import './bootstrap';
+import 'jquery';
+import 'bootstrap';
 import '@fullcalendar/core';
 import '@fullcalendar/interaction';
 import '@fullcalendar/daygrid';
