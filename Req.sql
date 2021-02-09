b61ed547eb6eb892d1b5f4ba1bd6e1a924b6e660
s.sorlin <s.sorlin@hotmail.com>
2021-02-09 15:46:38 +0100

    Modify Req.sql to add more req

--- Req.sql
+++ Req.sql
@@ -35,24 +35,108 @@ ORDER BY Stat1.month, Stat1.teleworked, Stat1.nameUser
 
 - D’avoir le pourcentage du temps de travail en entreprise ou en télétravail, par mois, et par salariés
 
-SELECT Stat1.nbrHoursNotTeleworked
+SELECT SUM(Stat1.nbrTime) AS nbrTime, Stat1.monthYear AS monthYear, Stat1.nameUser as nameUser, Stat1.teleworked as teleworked
 FROM (
-         SELECT SUM(TIME_TO_SEC(t.date_time_end) - TIME_TO_SEC(t.date_time_start)) AS nbrHoursTeleworked, CONCAT(MONTH(t.date_time_start), '/', YEAR(t.date_time_start)) AS monthYear, 			CONCAT(u.firstname, ' ', u.lastname) as nameUser
+         SELECT SUM(TIMEDIFF(t.date_time_end, t.date_time_start)) AS nbrTime, CONCAT(MONTH(t.date_time_start), '/', YEAR(t.date_time_start)) AS monthYear, CONCAT(u.firstname, ' ', u.lastname) as nameUser, wt.is_teleworked as teleworked
          FROM task t, user u, work_time wt
          WHERE t.user_id = u.id
-           AND t.work_time_id = wt.id
-           AND wt.is_teleworked = 1
+                   AND t.work_time_id = wt.id
+                   AND MONTH(t.date_time_start) = MONTH(t.date_time_end)
 
-         GROUP BY monthYear, nameUser
+GROUP BY monthYear, nameUser, teleworked
 
-         UNION
+UNION
 
-         SELECT SUM(TIME_TO_SEC(date_time_end) - TIME_TO_SEC(date_time_start)) AS nbrHoursNotTeleworked, CONCAT(MONTH(date_time_start), '/', YEAR(date_time_start)) AS monthYear, 						CONCAT(user.firstname, ' ', user.lastname) as nameUser
-         FROM task, user, work_time
-         WHERE task.user_id = user.id
-           AND work_time_id = work_time.id
-           AND is_teleworked = 0
+SELECT SUM(TIMEDIFF(ADDTIME(LAST_DAY(t.date_time_start), '24:00:00'), t.date_time_start)) AS nbrTime, CONCAT(MONTH(t.date_time_start), '/', YEAR(t.date_time_start)) AS monthYear, CONCAT(u.firstname, ' ', u.lastname) as nameUser, wt.is_teleworked as teleworked
+FROM task t, user u, work_time wt
+WHERE t.user_id = u.id
+          AND t.work_time_id = wt.id
+          AND MONTH(t.date_time_start) <> MONTH(t.date_time_end)
 
-         GROUP BY monthYear, nameUser
+GROUP BY monthYear, nameUser, teleworked
 
-     ) as Stat1
+UNION
+
+SELECT SUM(TIMEDIFF(t.date_time_end, ADDTIME(LAST_DAY(t.date_time_start), '24:00:00'))) AS nbrTime, CONCAT(MONTH(t.date_time_end), '/', YEAR(t.date_time_end)) AS monthYear, CONCAT(u.firstname, ' ', u.lastname) as nameUser, wt.is_teleworked as teleworked
+FROM task t, user u, work_time wt
+WHERE t.user_id = u.id
+          AND t.work_time_id = wt.id
+          AND MONTH(t.date_time_start) <> MONTH(t.date_time_end)
+
+GROUP BY monthYear, nameUser, teleworked
+    ) AS Stat1
+
+GROUP BY monthYear, nameUser, teleworked
+ORDER BY monthYear, nameUser, teleworked
+
+
+- D’avoir le pourcentage du temps de travail en entreprise ou en télétravail, par mois par « personnel non cadre » ou « personnel cadre »
+
+SELECT SUM(Stat1.nbrTime) AS nbrTime, Stat1.monthYear AS monthYear, Stat1.executive as executive, Stat1.teleworked as teleworked
+FROM (
+    SELECT SUM(TIMEDIFF(t.date_time_end, t.date_time_start)) AS nbrTime, CONCAT(MONTH(t.date_time_start), '/', YEAR(t.date_time_start)) AS monthYear, u.is_executive as executive, wt.is_teleworked as teleworked
+    FROM task t, user u, work_time wt
+    WHERE t.user_id = u.id
+        AND t.work_time_id = wt.id
+        AND MONTH(t.date_time_start) = MONTH(t.date_time_end)
+
+    GROUP BY monthYear, executive, teleworked
+
+    UNION
+
+    SELECT SUM(TIMEDIFF(ADDTIME(LAST_DAY(t.date_time_start), '24:00:00'), t.date_time_start)) AS nbrTime, CONCAT(MONTH(t.date_time_start), '/', YEAR(t.date_time_start)) AS monthYear, u.is_executive as executive, wt.is_teleworked as teleworked
+    FROM task t, user u, work_time wt
+    WHERE t.user_id = u.id
+        AND t.work_time_id = wt.id
+        AND MONTH(t.date_time_start) <> MONTH(t.date_time_end)
+
+    GROUP BY monthYear, executive, teleworked
+
+    UNION
+
+    SELECT SUM(TIMEDIFF(t.date_time_end, ADDTIME(LAST_DAY(t.date_time_start), '24:00:00'))) AS nbrTime, CONCAT(MONTH(t.date_time_end), '/', YEAR(t.date_time_end)) AS monthYear, u.is_executive as executive, wt.is_teleworked as teleworked
+    FROM task t, user u, work_time wt
+    WHERE t.user_id = u.id
+        AND t.work_time_id = wt.id
+        AND MONTH(t.date_time_start) <> MONTH(t.date_time_end)
+
+    GROUP BY monthYear, executive, teleworked
+    ) AS Stat1
+
+GROUP BY monthYear, executive, teleworked
+
+- D’avoir un récapitulatif mensuel cumulé des tâches effectuées par un salarié. Ce récapitulatif devra être sous le format suivant : Catégorie – Temps passé. Le détail des « commentaires » ne devra pas apparaître
+
+SELECT Stat1.userId as userId, Stat1.monthYear AS monthYear, Stat1.categ as categ, SUM(Stat1.nbrTime) AS nbrTime
+FROM (
+         SELECT SUM(TIMEDIFF(t.date_time_end, t.date_time_start)) AS nbrTime, CONCAT(MONTH(t.date_time_start), '/', YEAR(t.date_time_start)) AS monthYear, tc.name as categ, u.id as userId
+         FROM task t, user u, task_category tc
+         WHERE t.user_id = u.id
+                   AND t.task_category_id = tc.id
+                   AND MONTH(t.date_time_start) = MONTH(t.date_time_end)
+
+GROUP BY monthYear, categ, userId
+
+UNION
+
+SELECT SUM(TIMEDIFF(ADDTIME(LAST_DAY(t.date_time_start), '24:00:00'), t.date_time_start)) AS nbrTime, CONCAT(MONTH(t.date_time_start), '/', YEAR(t.date_time_start)) AS monthYear, tc.name as categ, u.id as userId
+FROM task t, user u, task_category tc
+WHERE t.user_id = u.id
+          AND t.task_category_id = tc.id
+          AND MONTH(t.date_time_start) <> MONTH(t.date_time_end)
+
+GROUP BY monthYear, categ, userId
+
+UNION
+
+SELECT SUM(TIMEDIFF(t.date_time_end, ADDTIME(LAST_DAY(t.date_time_start), '24:00:00'))) AS nbrTime, CONCAT(MONTH(t.date_time_end), '/', YEAR(t.date_time_end)) AS monthYear, tc.name as categ, u.id as userId
+FROM task t, user u, task_category tc
+WHERE t.user_id = u.id
+          AND t.task_category_id = tc.id
+          AND MONTH(t.date_time_start) <> MONTH(t.date_time_end)
+
+GROUP BY monthYear, categ, userId
+    ) AS Stat1
+
+GROUP BY monthYear, categ, userId
+ORDER BY monthYear, userId, categ
