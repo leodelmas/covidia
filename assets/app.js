1b7f66d7e19cc38b9ce9a143c5efe38d98b6690e
Léo Delmas <ldelmas@axome.com>
2021-03-02 08:37:46 +0100

    [*] Front, ajout d'icônes pour les boutons d'action

--- assets/app.js
+++ assets/app.js
@@ -3,12 +3,14 @@ import '@fullcalendar/core/main.min.css';
 import '@fullcalendar/timegrid/main.min.css';
 import '@fullcalendar/daygrid/main.min.css';
 import 'select2/dist/css/select2.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './styles/global.scss';
 
 // Dependancies
 import 'bootstrap';
 import 'select2';
 import 'flatpickr';
+import '@fortawesome/fontawesome-free/js/all'
 
 // Scripts
 import './scripts/global';
