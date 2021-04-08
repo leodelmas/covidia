0a119c67bedff22ac302a976ff9e730f0b8df2f0
Léo Delmas <ldelmas@axome.com>
2021-04-08 16:08:13 +0200

    [-] On supprimme l'initialisation des input type date

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -2,13 +2,4 @@ import {French} from "flatpickr/dist/l10n/fr";
 
 $(document).ready(function() {
     $('select[multiple=multiple]').select2();
-
-    $('input[type=date]').flatpickr({
-        allowInput: true,
-        altInput: true,
-        altFormat: "d/m/Y",
-        dateFormat: "Y-m-d",
-        locale: French,
-    });
-
 });
